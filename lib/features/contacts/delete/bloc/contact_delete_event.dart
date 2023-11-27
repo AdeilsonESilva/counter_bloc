2dff5c91693758a4d3dc50ec4766f985b93c95b9
Adeilson Silva <adeilson.silva@brainweb.com.br>
2023-11-27 16:24:52 -0300

    contact delete solution proposed

--- lib/features/contacts/delete/bloc/contact_delete_event.dart
+++
@@ -1,6 +0,0 @@
-part of 'contact_delete_bloc.dart';
-
-@freezed
-class ContactDeleteEvent with _$ContactDeleteEvent {
-  const factory ContactDeleteEvent.delete({required int id}) = _Delete;
-}
--- lib/features/contacts/delete/bloc/contact_delete_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'contact_delete_bloc.dart';
-
-@freezed
-class ContactDeleteState with _$ContactDeleteState {
-  const factory ContactDeleteState.initial() = _Initial;
-  const factory ContactDeleteState.loading() = _Loading;
-  const factory ContactDeleteState.error({required String message}) = _Error;
-  const factory ContactDeleteState.success() = _Success;
-}
